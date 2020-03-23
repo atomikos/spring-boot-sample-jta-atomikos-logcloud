0b36bf5de1357b616eba3b43885d3137780543a4
Pascal Leclercq <pascal.leclercq@gmail.com>
2020-03-23 16:06:58 +0100

    Updates with OWNING_RECOVERY_DOMAIN_NAME

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,2 @@
-INSERT INTO LOG_CLOUD_STARTUP_CONFIG (PROPERTY_NAME, PROPERTY_VALUE) values ('com.atomikos.icatch.max_timeout', '300000');
-INSERT INTO LOG_CLOUD_STARTUP_CONFIG (PROPERTY_NAME, PROPERTY_VALUE) values ('com.atomikos.icatch.tm_unique_name', 'logcloud');
-INSERT INTO LEADER (ACTIVE_JVM_ID) select ('NONE') WHERE NOT EXISTS (select 1 from LEADER); -- EXPIRY defaults to 0
+INSERT INTO LOG_CLOUD_STARTUP_CONFIG (OWNING_RECOVERY_DOMAIN_NAME, PROPERTY_NAME, PROPERTY_VALUE) values ('logcloud', 'com.atomikos.icatch.max_timeout', '300000');
+INSERT INTO LEADER (OWNING_RECOVERY_DOMAIN_NAME) select ('logcloud') WHERE NOT EXISTS (select 1 from LEADER); -- EXPIRY defaults to 0
